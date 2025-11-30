56a322e9f8b62e9aee4729df281a1fb5a6a7b5a7
Shallen-99 <shallenburks@gmail.com>
2025-11-29 19:01:35 -0500

    added and designed the clickable info boxes for the countries showing their flags and for right now empty information

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
-fetch("data.json").then((res) => res.json())
-.then(console.log)
+const container = document.querySelector(".container")
+
+fetch("data.json")
+    .then((res) => res.json())
+    .then((data) => {
+        data.forEach((country) => {
+            console.log(country)
+            const countryCard = document.createElement("a")
+            countryCard.classList.add("countryCard")
+
+            const countryText = `
+                <img src="${country.flags.svg}" alt="flag">
+                <div class="card-text">
+                    <h2 class="card-title"></h2>
+                    <p><b>Population:</b> </p>
+                    <p><b>Region:</b> </p>
+                    <p><b>Capital:</b> </p>
+                </div>
+            `
+
+            countryCard.innerHTML = countryText
+            container.append(countryCard)
+
+        })
+    })
+
