7a821177381fb0a4db0528e72121fafd594977bd
Shallen-99 <shallenburks@gmail.com>
2025-11-30 22:54:56 -0500

    Added dark mode toggle to country data page and cleaned up some js

--- src/country.js
+++ src/country.js
@@ -1,5 +1,7 @@
 const countryName = new URLSearchParams(location.search).get("name")
 const qs = (selector) => document.querySelector(selector)
+const darkMode = document.querySelector(".dark-mode")
+
 const element = {
     flag: qs(".country-details img"),
     name: qs(".country-details h1"),
@@ -9,7 +11,7 @@ const element = {
     subRegion: qs(".sub-region"),
     capital: qs(".capital"),
     tld: qs(".domain"),
-    currency: qs(".currencies"),
+    currencies: qs(".currencies"),
     languages: qs(".languages"),
     borders: qs(".border-countries"),
 }
@@ -23,16 +25,16 @@ fetch(`https://restcountries.com/v3.1/name/${countryName}?fullText=true`)
         element.name.textContent =  name.common
         element.population.textContent = population.toLocaleString("en-US")
         element.region.textContent = region
-        element.tld = tld?.join(", ")
+        element.tld.textContent = tld?.join(", ")
         element.subRegion.textContent = subregion || "N/A"
         element.capital.textContent = capital?.[0] || "N/A"
         element.nativeName.textContent = Object.values(name.nativeName ||  { x: { common: name.common } })[0].common
         element.currencies.textContent = currencies
-        ? Object.values(currencies).map((c) => c.name).join(", ")
-        : "N/A"
+            ? Object.values(currencies).map((c) => c.name).join(", ")
+            : "N/A"
         element.languages.textContent = languages
-        ? Object.values(languages).join(", ")
-        : "N/A";
+            ? Object.values(languages).join(", ")
+            : "N/A";
         if (borders) {
         borders.forEach((code) => {
             fetch(`https://restcountries.com/v3.1/alpha/${code}`)
@@ -41,8 +43,12 @@ fetch(`https://restcountries.com/v3.1/name/${countryName}?fullText=true`)
                 const tag = document.createElement("a");
                 tag.textContent = borderCountry.name.common;
                 tag.href = `country.html?name=${borderCountry.name.common}`;
-                el.borders.append(tag);
+                element.borders.append(tag);
             })
         })
         }
     })
+
+darkMode.addEventListener("click", () => {
+    document.body.classList.toggle("dark")
+})
